d0885786fbe86edff46766342efb62434bbd1f47
Gregory Casamento <greg.casamento@gmail.com>
2020-08-04 16:20:53 -0400

    Initial Commit

---
+++ NSStackView_test/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  NSStackView_test
+//
+//  Created by Gregory Casamento on 8/4/20.
+//  Copyright © 2020 Open Logic Corporation. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
