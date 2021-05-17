6e80d5f933eebe7ea4b5d0e32b49da2ae5937b71
Gregory Casamento <greg.casamento@gmail.com>
2021-05-17 06:51:05 -0400

    Additonal tests for stack view.

--- NSStackView_test/AppDelegate.m
+++ NSStackView_test/AppDelegate.m
@@ -11,14 +11,37 @@
 @interface AppDelegate ()
 
 @property (weak) IBOutlet NSWindow *window;
+@property IBOutlet NSStackView *verticalStack;
+@property IBOutlet NSStackView *horizontalStack;
+@property IBOutlet NSStackView *verticalGravityStack;
+
 @end
 
 @implementation AppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
     // Insert code here to initialize your application
+    NSRect f = NSMakeRect(0, 0,10, 30);
+    NSButton *v = [[NSButton alloc] initWithFrame: f];
+    [v setTitle: @"Button 2"];
+    [self.horizontalStack addView:v inGravity:NSStackViewGravityCenter];
+    
+    f = NSMakeRect(0, 0,10, 30);
+    v = [[NSButton alloc] initWithFrame: f];
+    [v setTitle: @"Button 3"];
+    [self.verticalStack addView:v inGravity:NSStackViewGravityCenter];
+    
+    NSLog(@"vertical subviews = %@", [_verticalStack subviews]);
+    NSLog(@"vertical arranged subviews = %@", [_verticalStack arrangedSubviews]);
+    
+    NSLog(@"vertical grav subviews = %@", [_verticalGravityStack subviews]);
+    NSLog(@"vertical grav arranged subviews = %@", [_verticalGravityStack arrangedSubviews]);
 }
 
+- (void) awakeFromNib
+{
+    // _verticalStack.arrangesAllSubviews = YES;
+}
 
 - (void)applicationWillTerminate:(NSNotification *)aNotification {
     // Insert code here to tear down your application
